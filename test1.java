8d79f9d1f62bcb2153441e3159edd727b091377c
riniv1234 <riniv1234@gmail.com>
2026-01-13 04:15:27 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello, Git repository!");
+        System.out.println("Hello, I have made the changes to pull repository!");
     }
 }
